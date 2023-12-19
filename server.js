e6861130ec72ea61e1b6bbbca986f7afe7c9b60f
athul <athulrsuresh@gmail.com>
2023-12-18 22:23:43 -0600

    fixed url?

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://music-recs-fe.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://music-recs-fe.vercel.app'],
     credentials: true
 }));
 app.use((req, res, next) => {
